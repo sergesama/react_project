411d6054ec9211db6beeee5d78b8100800e539bc
sergesama <sergesamas@gmail.com>
2020-09-03 22:11:50 +0300

    Add files via upload

---
+++ test/build/precache-manifest.c928baa3373117293b1e7e4724f55622.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533fb90429d4bb0206ea7d59c2a811d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac0995bb8103ecb06f0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a2efacaf967387b33e7",
+    "url": "/static/js/2.eff569e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eff569e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac0995bb8103ecb06f0",
+    "url": "/static/js/main.39e3e7af.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "a06d85a8453fd5898d3b55cea6293068",
+    "url": "/static/media/Aquarius.a06d85a8.jpg"
+  },
+  {
+    "revision": "b0b57d8b57b9f95ddc658c7ba081140b",
+    "url": "/static/media/Aries.b0b57d8b.jpg"
+  },
+  {
+    "revision": "59366ec66ab1c4eb84d0e0b61051e7ca",
+    "url": "/static/media/Cancer.59366ec6.jpg"
+  },
+  {
+    "revision": "237c90231229f50a2f3823f5ec50481a",
+    "url": "/static/media/Capricorn.237c9023.jpg"
+  },
+  {
+    "revision": "a015f94ae603d059641bcd42f44baf1d",
+    "url": "/static/media/Gemini.a015f94a.jpg"
+  },
+  {
+    "revision": "e8c7a86f6e05cae486bd33da7eb3a468",
+    "url": "/static/media/Leo.e8c7a86f.jpg"
+  },
+  {
+    "revision": "614366f1f2a27d32f96b67e02d373f31",
+    "url": "/static/media/Libra.614366f1.jpg"
+  },
+  {
+    "revision": "ea6e25d18e7df4e556509241bb0b8597",
+    "url": "/static/media/Pisces.ea6e25d1.jpg"
+  },
+  {
+    "revision": "1af53199b512440d718210c61cb114f3",
+    "url": "/static/media/Sagittarius.1af53199.jpg"
+  },
+  {
+    "revision": "f3391dacf8aa1ef8f755da3cab89fe92",
+    "url": "/static/media/Scorpio.f3391dac.jpg"
+  },
+  {
+    "revision": "cfda2264975793531fed26457b088f1c",
+    "url": "/static/media/Taurus.cfda2264.jpg"
+  },
+  {
+    "revision": "218e7cd76fc3d9ffe869d3d93bc46e2d",
+    "url": "/static/media/Virgo.218e7cd7.jpg"
+  }
+]);
